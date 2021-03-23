a59b514ffe6d028978df234329055d0d5ce61fad
rpanimation <rpanimation@icloud.com>
2021-03-23 17:30:28 +0100

    Dodano role.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("ZPI2020_IO1_Codesalmon, snsv-dy")
-print("ZPI2020_IO1_Codesalmon, piotr-s-nowak")
-print("ZPI2020_IO1_Codesalmon, RPAnimation")
-print("ZPI2020_IO1_Codesalmon, klima7")
+print("ZPI2020_IO1_Codesalmon, Scrum Master, snsv-dy")
+print("ZPI2020_IO1_Codesalmon, Developer, piotr-s-nowak")
+print("ZPI2020_IO1_Codesalmon, Developer, RPAnimation")
+print("ZPI2020_IO1_Codesalmon, Tester, klima7")
