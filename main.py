159bb1bd43acf153d984dd7d1f10a06e701cbb4d
rpanimation <rpanimation@icloud.com>
2021-04-13 16:32:17 +0200

    Zainicjalizowano klasę MArithmetics.

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+from IArithmetics import IArithmetics
+
+
+class MArithmetics(IArithmetics):
+    @staticmethod
+    def Addition():
+        pass
+
+    @staticmethod
+    def Difference():
+        pass
+
+    @staticmethod
+    def Division():
+        pass
+
+    @staticmethod
+    def Multiply():
+        pass
+
+
 print("ZPI2020_IO1_Codesalmon, Scrum Master, snsv-dy")
 print("ZPI2020_IO1_Codesalmon, Developer, piotr-s-nowak")
 print("ZPI2020_IO1_Codesalmon, Developer, RPAnimation")
