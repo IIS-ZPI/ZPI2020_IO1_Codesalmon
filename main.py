db1f06a19e49168c917b578a90dac9a933d5081e
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-04-13 17:32:32 +0200

    New feature added

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ class MArithmetics(IArithmetics):
     def Multiply(a, b):
 	return a * b
 
+    # klima7 feature
+
 
 print("ZPI2020_IO1_Codesalmon, Scrum Master, snsv-dy")
 print("ZPI2020_IO1_Codesalmon, Developer, piotr-s-nowak")
